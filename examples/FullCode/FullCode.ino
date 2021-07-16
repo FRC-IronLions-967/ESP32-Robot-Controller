5ecdc16f24d6ba27b0667e6d3d226afe8bcddbec
Nathan S <43659552+nastark123@users.noreply.github.com>
2021-07-15 22:05:24 -0500

    Added lookup table and added custom pin defs

--- examples/FullCode/FullCode.ino
+++ examples/FullCode/FullCode.ino
@@ -1,15 +1,28 @@
+#include <CustomBoardPinDefs.h>
 #include <GameController.h>
 #include <MotorController.h>
 #include <PWMServo.h>
 
 using namespace team967;
 
-MotorController leftController(15, 0);
-MotorController rightController(16, 1);
-MotorController arm(4, 4);
-PWMServo grabber(5, 3);
+MotorController leftController(P0, 0);
+MotorController rightController(P1, 1);
+MotorController arm(P2, 4);
+PWMServo grabber(P3, 3);
 
 GameController gameController("00:22:44:66:88:aa");
+
+const int8_t driveLookup[] = {0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18,
+                              18, 19, 19, 20, 20, 21, 21, 22, 23, 24, 24, 25, 25, 26, 26, 27, 27, 28, 28, 29, 29, 30, 30, 31, 31, 32, 34, 35, 37, 38, 40, 41,
+                              43, 44, 46, 47, 49, 50, 52, 53, 55, 56, 58, 59, 61, 62, 64, 65, 67, 68, 70, 71, 73, 74, 76, 77, 79, 80, 82, 83, 85, 86, 88, 89,
+                              91, 92, 94, 95, 97, 98, 100, 101, 103, 104, 106, 107, 109, 110, 112, 113, 115, 116, 118, 119, 121, 122, 124, 125, 127, 127, 127,
+                              127, 127};
+
+const int8_t turnLookup[] = {0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18,
+                              18, 19, 19, 20, 20, 21, 21, 22, 23, 24, 24, 25, 25, 26, 26, 27, 27, 28, 28, 29, 29, 30, 30, 31, 31, 32, 34, 35, 37, 38, 40, 41,
+                              43, 44, 46, 47, 49, 50, 52, 53, 55, 56, 58, 59, 61, 62, 64, 65, 67, 68, 70, 71, 73, 74, 76, 77, 79, 80, 82, 83, 85, 86, 88, 89,
+                              91, 92, 94, 95, 97, 98, 100, 101, 103, 104, 106, 107, 109, 110, 112, 113, 115, 116, 118, 119, 121, 122, 124, 125, 127, 127, 127,
+                              127, 127};
 
 void setup() {
   Serial.begin(115200);
@@ -36,8 +49,15 @@ void setup() {
 
 void loop() {
   if(gameController.isConnected()) {
-    short leftPower = gameController.getLeftStickY() + gameController.getRightStickX();
-    short rightPower = gameController.getLeftStickY() - gameController.getRightStickX();
+    short leftPower = ((-gameController.getLeftStickY() > 0) ? driveLookup[-gameController.getLeftStickY()] : -driveLookup[gameController.getLeftStickY()]) + 
+                      ((gameController.getRightStickX() > 0) ? turnLookup[gameController.getRightStickX()] : -turnLookup[-gameController.getRightStickX()]);
+    short rightPower = ((-gameController.getLeftStickY() > 0) ? driveLookup[-gameController.getLeftStickY()] : -driveLookup[gameController.getLeftStickY()]) - 
+                      ((gameController.getRightStickX() > 0) ? turnLookup[gameController.getRightStickX()] : -turnLookup[-gameController.getRightStickX()]);
+
+    Serial.print("Left Power: ");
+    Serial.print(leftPower);
+    Serial.print(" Right Power: ");
+    Serial.println(rightPower);
 
     // not sure if this is necessary, but check to make sure we haven't exceeded the range on an 8 bit signed int
     leftPower = (leftPower > 127) ? 127 : leftPower;
---
+++ src/CustomBoardPinDefs.h
@@ -0,0 +1,22 @@
+#ifndef CUSTOMBOARDPINDEFS_H
+#define CUSTOMBOARDPINDEFS_H
+
+namespace team967 {
+
+    // pin definitions for the gpio pins on the custom ESP32 board
+    const int P0 = 23;
+    const int P1 = 22;
+    const int P2 = 21;
+    const int P3 = 32;
+    const int P4 = 19;
+    const int P5 = 33;
+    const int P6 = 18;
+    const int P7 = 25;
+    const int P8 = 26;
+    const int P9 = 17;
+    const int P10 = 27;
+    const int P11 = 16;
+
+}
+
+#endif
