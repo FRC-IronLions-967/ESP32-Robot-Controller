2cc5d23e0a4b425df57e87fa5bc273355777e3fa
Nathan S <43659552+nastark123@users.noreply.github.com>
2021-07-22 21:21:09 -0500

    Tuned constants and fixed pin inconsistency

--- src/BatteryMonitor.cpp
+++ src/BatteryMonitor.cpp
@@ -1,16 +1,16 @@
 #include "BatteryMonitor.h"
 
-const uint16_t team967::BatteryMonitor::FULL_VAL = (1<<12);
+// these were determined experimentally
+const uint16_t team967::BatteryMonitor::FULL_VAL = 3250;
 
-const double team967::BatteryMonitor::FULL_VOLTAGE = 12.0;
-
+const double team967::BatteryMonitor::FULL_VOLTAGE = 13.5;
 bool team967::BatteryMonitor::initialized = false;
 
 double team967::BatteryMonitor::getBatteryVoltage() {
     if(!initialized) {
         adc1_config_width(ADC_WIDTH_BIT_12);
-        adc1_config_channel_atten(ADC_CHANNEL_5, ADC_ATTEN_DB_11);
+        adc1_config_channel_atten(ADC1_CHANNEL_6, ADC_ATTEN_DB_11);
         initialized = true;
     }
-    return (((double) adc1_get_raw(ADC_CHANNEL_5)) / ((double) FULL_VAL)) * FULL_VOLTAGE;
+    return (((double) adc1_get_raw(ADC1_CHANNEL_6)) / ((double) FULL_VAL)) * FULL_VOLTAGE;
 }
